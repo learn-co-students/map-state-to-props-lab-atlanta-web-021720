0ff847a9168739f56ca83d28a78bba7e3c700c22
chan <thechanmoon@gmail.com>
2020-05-07 21:07:10 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -6,12 +6,37 @@ import manageUsers from './reducers/manageUsers'
 
 
 // add imports and code
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
+let store = createStore(
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   // add imports and code
-  <App />
+  <Provider store ={store}>
+    <App />
+  </Provider>
   // add imports and code
   ,
   document.getElementById('root')
 );
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App'
+// import { Provider } from 'react-redux'
+// import { createStore } from 'redux'
+// import manageUsers from './reducers/manageUsers'
+
+// let store = createStore(manageUsers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// );
